c599a54fad124128d1a4f9d87a121be39818440c
waldo <evgenijtser@gmail.com>
2024-05-05 12:49:17 +0300

    Change root of the project

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import { resolve } from 'path';
 import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
+    root: resolve(__dirname, 'src'),
+    build: {
+        outDir: resolve(__dirname, 'dist')
+    },
     server: {
         port: 3000,
     },
