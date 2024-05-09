83644c63b30ab8d9d32a2e0e3c351d93ca3cd1e0
waldo <evgenijtser@gmail.com>
2024-05-09 23:16:24 +0300

    Remove libs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import { resolve } from 'path';
 import handlebars from 'vite-plugin-handlebars';
-import autoprefixer from "autoprefixer";
 
 export default defineConfig({
     root: resolve(__dirname, 'src'),
