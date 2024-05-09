2ef86531e9ca9b758aef9cb38918c199e83c2870
waldo <evgenijtser@gmail.com>
2024-05-09 14:50:03 +0300

    Add server js

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.static('dist'))
+
+app.get("/", (req, res) => {
+    res.sendFile('dist/index.html')
+})
+
+app.listen(3000);
