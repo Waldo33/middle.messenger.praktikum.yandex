3aa89b40266f79e7203c71a545db45ca7c4adb85
waldo <evgenijtser@gmail.com>
2024-07-06 17:20:26 +0300

    Remove redundant import

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import { resolve } from 'path';
-import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
     root: resolve(__dirname, 'src'),
