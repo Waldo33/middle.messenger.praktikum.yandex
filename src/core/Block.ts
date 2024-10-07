fc4abc7116518ec0ab6020ca00bc42c9b2a7d787
waldo <evgenijtser@gmail.com>
2024-10-07 21:06:20 +0300

    Test lint-staged

--- src/core/Block.ts
+++ src/core/Block.ts
@@ -1,4 +1,4 @@
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
 import Handlebars from 'handlebars';
 import EventBus from './EventBus';
 import { Nullable, Values } from '../types';
